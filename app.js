8b9e787b92f9aa492a74fdcc69a8dc76a3a6a171
jpdmostwanted <32849868+jpdmostwanted@users.noreply.github.com>
2017-11-06 01:41:03 +0530

    Added CORS Support

--- app.js
+++ app.js
@@ -5,6 +5,24 @@ var mongoose = require('mongoose');
 Genres = require('./models/genre');
 Books = require('./models/book');
 app.use(bodyParse.json());
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 //mongoose.connect('mongodb://localhost/bookstore');
 mongoose.connect('mongodb://jpdmw:cybersh0t@ds245615.mlab.com:45615/bookstore');
 //var db = mongoose.connection;
